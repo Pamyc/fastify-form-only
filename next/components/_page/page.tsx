19f68c1748bd9e520fb4536fc56c9cc735bba444
Pamyc <163673087+Pamyc@users.noreply.github.com>
2025-10-07 15:09:48 +0300

    Update page.tsx

--- next/components/_page/page.tsx
+++ next/components/_page/page.tsx
@@ -1,13 +1,15 @@
 // next/components/_page/page.tsx
 'use client'
 
-import { ThemeProvider } from '@/components/theme-provider' // <- изменил на именованный импорт для консистентности
-import { EventAnalysisProvider } from '@/components/event-analysis-provider'
-import { EventAnalysisApp } from '@/components/event-analysis-app' // <- ГЛАВНОЕ ИСПРАВЛЕНИЕ ЗДЕСЬ
+// Заменяем алиасы на относительные пути для исправления ошибки сборки
+import { ThemeProvider } from '../theme-provider'
+import { EventAnalysisApp } from '../event-analysis-app'
+
+// EventAnalysisProvider уже находится внутри EventAnalysisApp, поэтому здесь он не нужен
 
 export default function PageAdapter() {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider>
       <EventAnalysisApp />
     </ThemeProvider>
   )
