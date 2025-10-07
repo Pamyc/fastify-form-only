cd858fe9d5ce4b788227d518f9e917a4391414b2
Pamyc <163673087+Pamyc@users.noreply.github.com>
2025-10-07 13:36:27 +0300

    Update page.tsx

--- next/components/_page/page.tsx
+++ next/components/_page/page.tsx
@@ -1,9 +1,9 @@
 // next/components/_page/page.tsx
 'use client';
 
-import ThemeProvider from '../theme-provider';
-import { EventAnalysisProvider } from '../event-analysis-provider';
-import EventAnalysisApp from '../event-analysis-app';
+import ThemeProvider from '@/theme-provider';
+import { EventAnalysisProvider } from '@/event-analysis-provider';
+import EventAnalysisApp from '@/event-analysis-app';
 
 export default function PageAdapter() {
   return (
