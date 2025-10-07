98c1401a94c9fe2402744b0c66a6f192c8343394
Pamyc <163673087+Pamyc@users.noreply.github.com>
2025-10-07 14:59:38 +0300

    Update page.tsx

--- next/components/_page/page.tsx
+++ next/components/_page/page.tsx
@@ -1,16 +1,14 @@
 // next/components/_page/page.tsx
 'use client'
 
-import ThemeProvider from '@/components/theme-provider'
+import { ThemeProvider } from '@/components/theme-provider' // <- изменил на именованный импорт для консистентности
 import { EventAnalysisProvider } from '@/components/event-analysis-provider'
-import EventAnalysisApp from '@/components/event-analysis-app'
+import { EventAnalysisApp } from '@/components/event-analysis-app' // <- ГЛАВНОЕ ИСПРАВЛЕНИЕ ЗДЕСЬ
 
 export default function PageAdapter() {
   return (
-    <ThemeProvider>
-      <EventAnalysisProvider>
-        <EventAnalysisApp />
-      </EventAnalysisProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <EventAnalysisApp />
     </ThemeProvider>
   )
 }
