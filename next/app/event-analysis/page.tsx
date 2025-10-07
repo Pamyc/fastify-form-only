8382d99107d0e480dd01477c01655db650d4d1ea
Pamyc <163673087+Pamyc@users.noreply.github.com>
2025-10-07 12:08:07 +0300

    Update page.tsx

--- next/app/event-analysis/page.tsx
+++ next/app/event-analysis/page.tsx
@@ -1,5 +1,6 @@
-import Imported from '../components/_page/page';
-export default function EventAnalysisPage(){
+import Imported from '../../components/_page/page';
+
+export default function EventAnalysisPage() {
   return (
     <main style={{ display:'grid', gap:12 }}>
       <h1>Event Analysis</h1>
